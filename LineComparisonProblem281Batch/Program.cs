586a74532eb01eff03e7fc73f2f8c14caf70040e
unknown <amoljadhav>
2023-05-15 23:55:13 +0530

    [Amol] calculateLengthofLine

--- LineComparisonProblem281Batch/Program.cs
+++ LineComparisonProblem281Batch/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Line Comparison Problem!");
+            Console.WriteLine("Enter the coordinates of the first point (x1, y1):");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the coordinates of the second point (x2, y2):");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double length = CalculateLineLength(x1, y1, x2, y2);
+
+            Console.WriteLine("The Length of Line is: " + length);
         }
+
+        private static double CalculateLineLength(int x1, int y1, int x2, int y2)
+        {
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            return length;
+        }
+
     }
+    
 }
