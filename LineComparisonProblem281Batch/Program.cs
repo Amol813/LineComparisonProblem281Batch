71dceb8373bb61c49eed907dea68ff86181aa827
unknown <amoljadhav>
2023-05-16 00:17:35 +0530

    [Amol] CompareTwoLines

--- LineComparisonProblem281Batch/Program.cs
+++ LineComparisonProblem281Batch/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-public class Program
+﻿public class Program
 {
     private static void Main(string[] args)
     {
@@ -15,7 +13,7 @@ public class Program
         double x2 = Convert.ToDouble(Console.ReadLine());
         double y2 = Convert.ToDouble(Console.ReadLine());
 
-        double length1 = CalculateLineLength(x1, y1, x2, y2);
+        Line line1 = new Line(x1, y1, x2, y2);
 
         // Line 2
         Console.WriteLine("Enter the coordinates of the first point of Line 2 (a1, b1):");
@@ -26,19 +24,26 @@ public class Program
         double a2 = Convert.ToDouble(Console.ReadLine());
         double b2 = Convert.ToDouble(Console.ReadLine());
 
-        double length2 = CalculateLineLength(a1, b1, a2, b2);
+        Line line2 = new Line(a1, b1, a2, b2);
+
+        double length1 = line1.CalculateLength();
+        double length2 = line2.CalculateLength();
 
         Console.WriteLine("The Length of Line 1 is: " + length1);
         Console.WriteLine("The Length of Line 2 is: " + length2);
 
-        // Check equality using the equals method
-        bool areEqual = length1.Equals(length2);
-        Console.WriteLine("Are the lines equal? " + areEqual);
-    }
-
-    private static double CalculateLineLength(double x1, double y1, double x2, double y2)
-    {
-        double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-        return length;
+        // Compare line lengths
+        if (length1 > length2)
+        {
+            Console.WriteLine("Line 1 is greater than Line 2.");
+        }
+        else if (length1 < length2)
+        {
+            Console.WriteLine("Line 1 is less than Line 2.");
+        }
+        else
+        {
+            Console.WriteLine("Line 1 is equal to Line 2.");
+        }
     }
 }
