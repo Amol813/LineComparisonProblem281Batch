9cf3913bc3d7de45144643385c5e5b948ebe74f1
unknown <amoljadhav>
2023-05-16 00:05:22 +0530

    [Amol] EqualityOfTwoLines

--- LineComparisonProblem281Batch/Program.cs
+++ LineComparisonProblem281Batch/Program.cs
@@ -1,29 +1,44 @@
-﻿namespace LineComparisonProblem281Batch
+﻿using System;
+
+public class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to the Line Comparison Problem!");
-            Console.WriteLine("Enter the coordinates of the first point (x1, y1):");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            int y1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Welcome to Line Comparison Computation Program!");
 
-            Console.WriteLine("Enter the coordinates of the second point (x2, y2):");
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            int y2 = Convert.ToInt32(Console.ReadLine());
+        // Line 1
+        Console.WriteLine("Enter the coordinates of the first point of Line 1 (x1, y1):");
+        double x1 = Convert.ToDouble(Console.ReadLine());
+        double y1 = Convert.ToDouble(Console.ReadLine());
 
-            double length = CalculateLineLength(x1, y1, x2, y2);
+        Console.WriteLine("Enter the coordinates of the second point of Line 1 (x2, y2):");
+        double x2 = Convert.ToDouble(Console.ReadLine());
+        double y2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("The Length of Line is: " + length);
-        }
+        double length1 = CalculateLineLength(x1, y1, x2, y2);
 
-        private static double CalculateLineLength(int x1, int y1, int x2, int y2)
-        {
-            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            return length;
-        }
+        // Line 2
+        Console.WriteLine("Enter the coordinates of the first point of Line 2 (a1, b1):");
+        double a1 = Convert.ToDouble(Console.ReadLine());
+        double b1 = Convert.ToDouble(Console.ReadLine());
 
+        Console.WriteLine("Enter the coordinates of the second point of Line 2 (a2, b2):");
+        double a2 = Convert.ToDouble(Console.ReadLine());
+        double b2 = Convert.ToDouble(Console.ReadLine());
+
+        double length2 = CalculateLineLength(a1, b1, a2, b2);
+
+        Console.WriteLine("The Length of Line 1 is: " + length1);
+        Console.WriteLine("The Length of Line 2 is: " + length2);
+
+        // Check equality using the equals method
+        bool areEqual = length1.Equals(length2);
+        Console.WriteLine("Are the lines equal? " + areEqual);
     }
-    
+
+    private static double CalculateLineLength(double x1, double y1, double x2, double y2)
+    {
+        double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        return length;
+    }
 }
